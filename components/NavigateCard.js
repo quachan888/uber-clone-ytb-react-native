6a3db378eb08caa43848cd19c576c8708ad1c778
An Quach <quachan888@gmail.com>
2022-02-26 06:14:10 +0700

    Add bottom button Ride, Eats

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import tailwind from 'twrnc';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux';
 import { setDestination } from '../slices/navSlice';
 import { useNavigation } from '@react-navigation/native';
 import NavFavourites from './NavFavourites';
+import { Icon } from 'react-native-elements';
 
 const NavigateCard = () => {
     const dispatch = useDispatch();
@@ -42,6 +43,23 @@ const NavigateCard = () => {
                     />
                 </View>
                 <NavFavourites />
+            </View>
+            <View
+                style={tailwind`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100 `}
+            >
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('RideOptionsCard ')}
+                    style={tailwind`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+                >
+                    <Icon name="car" type="font-awesome" color="white" size={16} />
+                    <Text style={tailwind`text-white text-center`}>Rides</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={tailwind`flex flex-row  justify-between w-24 px-4 py-3 rounded-full`}
+                >
+                    <Icon name="fast-food-outline" type="ionicon" color="black" size={16} />
+                    <Text style={tailwind` text-center`}>Eats</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
