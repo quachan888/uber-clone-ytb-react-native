acfb10b1e01ad21fb90e4a70bac14e4d562e1449
An Quach <quachan888@gmail.com>
2022-02-26 06:02:57 +0700

    KeyboardAvoidingView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
@@ -13,22 +13,27 @@ export default function App() {
     return (
         <Provider store={store}>
             <NavigationContainer>
-                {/* <Text>{GOOGLE_MAPS_APIKEY}</Text> */}
                 <SafeAreaProvider>
-                    <Stack.Navigator>
-                        <Stack.Screen
-                            name="HomeScreen"
-                            component={HomeScreen}
-                            options={{
-                                headerShown: false
-                            }}
-                        />
-                        <Stack.Screen
-                            name="MapScreen"
-                            component={MapScreen}
-                            options={{ headerShown: false }}
-                        />
-                    </Stack.Navigator>
+                    <KeyboardAvoidingView
+                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                        style={{ flex: 1 }}
+                        keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+                    >
+                        <Stack.Navigator>
+                            <Stack.Screen
+                                name="HomeScreen"
+                                component={HomeScreen}
+                                options={{
+                                    headerShown: false
+                                }}
+                            />
+                            <Stack.Screen
+                                name="MapScreen"
+                                component={MapScreen}
+                                options={{ headerShown: false }}
+                            />
+                        </Stack.Navigator>
+                    </KeyboardAvoidingView>
                 </SafeAreaProvider>
             </NavigationContainer>
         </Provider>
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -6,6 +6,7 @@ import { GOOGLE_MAPS_APIKEY } from '@env';
 import { useDispatch } from 'react-redux';
 import { setDestination } from '../slices/navSlice';
 import { useNavigation } from '@react-navigation/native';
+import NavFavourites from './NavFavourites';
 
 const NavigateCard = () => {
     const dispatch = useDispatch();
@@ -40,6 +41,7 @@ const NavigateCard = () => {
                         }}
                     />
                 </View>
+                <NavFavourites />
             </View>
         </View>
     );
